8e3a01a13ffeafb9c7a63dd65b3816f36879cbf1
kkdashu <wmeng17@gmail.con>
2024-10-22 15:10:01 +0800

    add denols

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,12 +1,32 @@
 local nvim_lsp = require('lspconfig')
+local configs = require('lspconfig.configs')
+local util = require('lspconfig.util')
 
 local on_attach = require('lsp_on_attach').on_attach
 
 -- languages
-local languages = {'pyright', 'gopls', 'rust_analyzer', 'ts_ls', 'lua_ls', 'dartls', 'html', 'marksman', 'bufls'}
+local languages = {'pyright', 'gopls', 'rust_analyzer', 'ts_ls', 'lua_ls', 'dartls', 'html', 'marksman', 'protobuf-language-server', 'sqls', 'denols'}
 
 -- setup nvim-cmp
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local key = 'protobuf-language-server'
+
+configs[key] = {
+    default_config = {
+        cmd = { '~/.gvm/pkgsets/go1.22/global/bin/protobuf-language-server' },
+        filetypes = { 'proto', 'cpp' },
+        root_fir = util.root_pattern('.git'),
+        single_file_support = true,
+        settings = {
+            -- ["additional-proto-dirs"] = [
+            --     -- path to additional protobuf directories
+            --     -- "vendor",
+            --     -- "third_party",
+            -- ]
+        },
+    }
+}
 
 for _, lsp in ipairs(languages) do
   local config = {
@@ -31,6 +51,11 @@ for _, lsp in ipairs(languages) do
         buildFlags = {'-tags=integration'}
       }
     }
+  elseif lsp == 'ts_ls' then
+    config.root_dir = nvim_lsp.util.root_pattern("package.json")
+    config.single_file_support = false
+  elseif lsp == 'denols' then
+    config.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc")
   end
   nvim_lsp[lsp].setup(config)
 end
